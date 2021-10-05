61288b1f68d5079e1ca5661c7e9c1c4de54d9fee
QuangDat2412 <ledatumltk123@gmail.com>
2021-10-05 13:18:28 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
     const $ = document.querySelector.bind(document);
     const $$ = document.querySelectorAll.bind(document);
 
@@ -11,8 +11,8 @@ window.addEventListener('load', function(){
     const audio = $("#audio");
     const playBtn = $(".btn-toggle-play");
     const progress = $("#progress");
-    const currentTime = $('.current-time');
-    const musicDuration = $('.song-duration');
+    const currentTime = $(".current-time");
+    const musicDuration = $(".song-duration");
     const prevBtn = $(".btn-prev");
     const nextBtn = $(".btn-next");
     const randomBtn = $(".btn-random");
@@ -21,98 +21,98 @@ window.addEventListener('load', function(){
 
     var formatTime = (time) => {
         let min = Math.floor(time / 60);
-        if(min < 10){
+        if (min < 10) {
             min = `0${min}`;
         }
         let sec = Math.floor(time % 60);
-        if(sec < 10){
+        if (sec < 10) {
             sec = `0${sec}`;
         }
         return `${min} : ${sec}`;
-    }
+    };
     const app = {
         currentIndex: 0,
         isPlaying: false,
         isRandom: false,
-        isRepeat:false,
-        config: JSON.parse(localStorage.getItem(PlAYER_STORAGE_KEY))||{},
-        setConfig:function (key,value) {
+        isRepeat: false,
+        config: JSON.parse(localStorage.getItem(PlAYER_STORAGE_KEY)) || {},
+        setConfig: function (key, value) {
             this.config[key] = value;
-            localStorage.setItem(PlAYER_STORAGE_KEY, JSON.stringify(this.config))
+            localStorage.setItem(PlAYER_STORAGE_KEY, JSON.stringify(this.config));
         },
         songs: [
             {
-                name: 'Nevada',
-                singer: 'Vicetone',
-                path: 'https://aredir.nixcdn.com/NhacCuaTui924/Nevada-Vicetone-4494556.mp3?st=_IjpS9u0LjapNgzm058wVw&e=1623143773',
-                image: 'https://i.pinimg.com/originals/f8/6f/33/f86f3378e656883b33594f06d78d1634.jpg',
+                name: "Nevada",
+                singer: "Vicetone",
+                path: "https://aredir.nixcdn.com/NhacCuaTui924/Nevada-Vicetone-4494556.mp3?st=_IjpS9u0LjapNgzm058wVw&e=1623143773",
+                image: "https://i.pinimg.com/originals/f8/6f/33/f86f3378e656883b33594f06d78d1634.jpg",
             },
             {
-                name: 'Light It Up',
-                singer: 'Robin Hustin x TobiMorrow',
-                path: 'https://aredir.nixcdn.com/NhacCuaTui968/LightItUp-RobinHustinTobimorrowJex-5619031.mp3?st=kzpVQ5kKnf2LlcAqM6lnxg&e=1623143881',
-                image: 'https://avatar-ex-swe.nixcdn.com/song/2019/01/08/1/3/d/a/1546913843457_640.jpg',
+                name: "Light It Up",
+                singer: "Robin Hustin x TobiMorrow",
+                path: "https://aredir.nixcdn.com/NhacCuaTui968/LightItUp-RobinHustinTobimorrowJex-5619031.mp3?st=kzpVQ5kKnf2LlcAqM6lnxg&e=1623143881",
+                image: "https://avatar-ex-swe.nixcdn.com/song/2019/01/08/1/3/d/a/1546913843457_640.jpg",
             },
             {
-                name: 'Yoru ni kakeru',
-                singer: 'YOASOBI',
-                path: 'https://aredir.nixcdn.com/NhacCuaTui992/YoruNiKakeru-YOASOBI-6149490.mp3?st=68hnFhtGF6RukKDcDcW9Mw&e=1623132179',
-                image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16788ee5-3436-474a-84fd-6616063a1a9a/de2f4eq-bc67fa17-8dae-46a9-b85d-fe8082c34841.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2Nzg4ZWU1LTM0MzYtNDc0YS04NGZkLTY2MTYwNjNhMWE5YVwvZGUyZjRlcS1iYzY3ZmExNy04ZGFlLTQ2YTktYjg1ZC1mZTgwODJjMzQ4NDEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dABuqANeQEs6FBfslZHdG1lW_gDwzf61yqiSABROSx0',
+                name: "Yoru ni kakeru",
+                singer: "YOASOBI",
+                path: "https://aredir.nixcdn.com/NhacCuaTui992/YoruNiKakeru-YOASOBI-6149490.mp3?st=68hnFhtGF6RukKDcDcW9Mw&e=1623132179",
+                image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16788ee5-3436-474a-84fd-6616063a1a9a/de2f4eq-bc67fa17-8dae-46a9-b85d-fe8082c34841.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2Nzg4ZWU1LTM0MzYtNDc0YS04NGZkLTY2MTYwNjNhMWE5YVwvZGUyZjRlcS1iYzY3ZmExNy04ZGFlLTQ2YTktYjg1ZC1mZTgwODJjMzQ4NDEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dABuqANeQEs6FBfslZHdG1lW_gDwzf61yqiSABROSx0",
             },
             {
-                name: 'Muộn rồi mà sao còn',
-                singer: 'Sơn Tùng M-TP',
-                path: 'https://aredir.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=w9AA-eyRI7yD_VYGfvVWeQ&e=1623141624',
-                image: 'https://pbs.twimg.com/media/Ez5jRyVVgAQN6Kh.jpg',
+                name: "Muộn rồi mà sao còn",
+                singer: "Sơn Tùng M-TP",
+                path: "https://aredir.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=w9AA-eyRI7yD_VYGfvVWeQ&e=1623141624",
+                image: "https://pbs.twimg.com/media/Ez5jRyVVgAQN6Kh.jpg",
             },
             {
-                name: 'See You Again',
-                singer: 'Charlie Puth ft Wiz Khalifa',
-                path: 'https://aredir.nixcdn.com/NhacCuaTui894/SeeYouAgain-KurtSchneiderEppicAlexGoot-3888930.mp3?st=1q73myBS8FKr8Rx0snpMJw&e=1623144094',
-                image: 'https://nghiennhac.com/wp-content/uploads/2020/09/see-you-again-0.jpg',
+                name: "See You Again",
+                singer: "Charlie Puth ft Wiz Khalifa",
+                path: "https://aredir.nixcdn.com/NhacCuaTui894/SeeYouAgain-KurtSchneiderEppicAlexGoot-3888930.mp3?st=1q73myBS8FKr8Rx0snpMJw&e=1623144094",
+                image: "https://nghiennhac.com/wp-content/uploads/2020/09/see-you-again-0.jpg",
             },
-        
+
             {
-                name: 'Symphony',
-                singer: 'Clean Bandit',
-                path: 'https://aredir.nixcdn.com/Sony_Audio37/Symphony-CleanBanditZaraLarsson-4822950.mp3?st=sPgJSXtRXYpT_rznXyez6g&e=1623144426',
-                image: 'https://i.ytimg.com/vi/PIf9GvWaxQQ/maxresdefault.jpg',
-            },
-            {
-                name: 'Waiting For Love',
-                singer: 'Avicii',
-                path: 'https://aredir.nixcdn.com/Unv_Audio45/WaitingForLove-Avicii-4203283.mp3?st=mXGv6kIqbxg_coAyUqzlnw&e=1623144462',
-                image: 'https://i.ytimg.com/vi/Hmbm3G-Q444/maxresdefault.jpg',
+                name: "Symphony",
+                singer: "Clean Bandit",
+                path: "https://aredir.nixcdn.com/Sony_Audio37/Symphony-CleanBanditZaraLarsson-4822950.mp3?st=sPgJSXtRXYpT_rznXyez6g&e=1623144426",
+                image: "https://i.ytimg.com/vi/PIf9GvWaxQQ/maxresdefault.jpg",
             },
             {
-                name: 'Alone',
-                singer: 'Marshmello',
-                path: 'https://aredir.nixcdn.com/NhacCuaTui927/Alone-Marshmello-4456939.mp3?st=RTsMC9tNcKEi8fd0iKtdaA&e=1623144502',
-                image: 'https://i.ytimg.com/vi/UNB8F0ObA4g/maxresdefault.jpg',
+                name: "Waiting For Love",
+                singer: "Avicii",
+                path: "https://aredir.nixcdn.com/Unv_Audio45/WaitingForLove-Avicii-4203283.mp3?st=mXGv6kIqbxg_coAyUqzlnw&e=1623144462",
+                image: "https://i.ytimg.com/vi/Hmbm3G-Q444/maxresdefault.jpg",
             },
             {
-                name: 'Something Just Like This',
-                singer: 'The Chainsmokers & Coldplay',
-                path: 'https://aredir.nixcdn.com/Sony_Audio39/SomethingJustLikeThis-TheChainsmokersColdplay-5337136.mp3?st=VQuH6VgNsPlBizbk-c7n3w&e=1623144556',
-                image: 'https://avatar-ex-swe.nixcdn.com/song/2017/11/07/a/1/4/5/1510038809679_640.jpg',
+                name: "Alone",
+                singer: "Marshmello",
+                path: "https://aredir.nixcdn.com/NhacCuaTui927/Alone-Marshmello-4456939.mp3?st=RTsMC9tNcKEi8fd0iKtdaA&e=1623144502",
+                image: "https://i.ytimg.com/vi/UNB8F0ObA4g/maxresdefault.jpg",
             },
             {
-                name: 'Độ Tộc 2',
-                singer: 'Masew, Độ Mixi, Phúc Du, Pháo',
-                path: 'https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/DoToc2-MasewDoMixiPhucDuPhao-7064730.mp3?st=g1J0UIwLpxqVYVhbSB-WcA&e=1630840299',
-                image: 'https://avatar-ex-swe.nixcdn.com/song/2021/08/10/b/2/e/0/1628579601228_500.jpg',
+                name: "Something Just Like This",
+                singer: "The Chainsmokers & Coldplay",
+                path: "https://aredir.nixcdn.com/Sony_Audio39/SomethingJustLikeThis-TheChainsmokersColdplay-5337136.mp3?st=VQuH6VgNsPlBizbk-c7n3w&e=1623144556",
+                image: "https://avatar-ex-swe.nixcdn.com/song/2017/11/07/a/1/4/5/1510038809679_640.jpg",
             },
             {
-                name: 'Sugar',
-                singer: 'Maroon 5',
-                path: 'https://aredir.nixcdn.com/Unv_Audio73/Sugar-Maroon5-3338455.mp3?st=3FUWEyikJePPeAuREUcw9g&e=1623144644',
-                image: 'https://avatar-ex-swe.nixcdn.com/song/2018/06/22/0/c/c/b/1529655970762_500.jpg',
+                name: "Độ Tộc 2",
+                singer: "Masew, Độ Mixi, Phúc Du, Pháo",
+                path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/DoToc2-MasewDoMixiPhucDuPhao-7064730.mp3?st=g1J0UIwLpxqVYVhbSB-WcA&e=1630840299",
+                image: "https://avatar-ex-swe.nixcdn.com/song/2021/08/10/b/2/e/0/1628579601228_500.jpg",
+            },
+            {
+                name: "Sugar",
+                singer: "Maroon 5",
+                path: "https://aredir.nixcdn.com/Unv_Audio73/Sugar-Maroon5-3338455.mp3?st=3FUWEyikJePPeAuREUcw9g&e=1623144644",
+                image: "https://avatar-ex-swe.nixcdn.com/song/2018/06/22/0/c/c/b/1529655970762_500.jpg",
             },
         ],
-        render: function(){
+        render: function () {
             const htmls = this.songs.map((song, index) => {
                 return `
-                    <div class="song ${index===this.currentIndex ? 'active' : ''}" data-index="${index}">
+                    <div class="song ${index === this.currentIndex ? "active" : ""}" data-index="${index}">
                         <div class="thumb"
                             style="background-image: url('${song.image}')">
                         </div>
@@ -125,25 +125,23 @@ window.addEventListener('load', function(){
                         </div>
                     </div>
                 `;
-            })
-            $('.playlist').innerHTML = htmls.join('');
+            });
+            $(".playlist").innerHTML = htmls.join("");
         },
-        defineProperties () {
-            Object.defineProperty(this, 'currentSong', {
+        defineProperties() {
+            Object.defineProperty(this, "currentSong", {
                 get: function () {
-                    return this.songs[this.currentIndex]
-                }
-            })
+                    return this.songs[this.currentIndex];
+                },
+            });
         },
         handleEvents: function () {
             const _this = this;
             const cdWidth = cd.offsetWidth;
             // Xử lý CD quay / dừng
-            const cdThumbAnimate = cdThumb.animate([
-                {transform:'rotate(360deg)'}
-            ],{
-                duration:10000,
-                iterations: Infinity
+            const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+                duration: 10000,
+                iterations: Infinity,
             });
             cdThumbAnimate.pause();
             // Xử lý phóng to / thu nhỏ CD
@@ -153,7 +151,7 @@ window.addEventListener('load', function(){
 
                 cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0;
                 cd.style.opacity = newCdWidth / cdWidth;
-            }
+            };
             // Xử lý khi click play
             playBtn.onclick = function () {
                 if (_this.isPlaying) {
@@ -161,106 +159,105 @@ window.addEventListener('load', function(){
                 } else {
                     audio.play();
                 }
-            }
+            };
             // Khi song được play
             audio.onplay = function () {
                 _this.isPlaying = true;
                 player.classList.add("playing");
                 cdThumbAnimate.play();
                 _this.scrollToActiveSong();
-            }
+            };
             // Khi song bị pause
             audio.onpause = function () {
                 _this.isPlaying = false;
                 player.classList.remove("playing");
                 cdThumbAnimate.pause();
-            }
+            };
             // Khi tiến độ bài hát thay đổi
             audio.ontimeupdate = function () {
-                if(audio.duration){
-                    const progressPercent = Math.round(audio.currentTime / audio.duration *100);
+                if (audio.duration) {
+                    const progressPercent = Math.round((audio.currentTime / audio.duration) * 100);
                     progress.value = progressPercent;
                 }
                 currentTime.innerHTML = formatTime(audio.currentTime);
-            }
-            progress.oninput = function(e){
+            };
+            progress.oninput = function (e) {
                 // từ số phần trăm của giây convert sang giây
-                const seekTime = audio.duration / 100 * e.target.value; 
+                const seekTime = (audio.duration / 100) * e.target.value;
                 audio.currentTime = seekTime;
                 audio.play();
-            }
+            };
             // Khi next song
-            nextBtn.onclick = function(){
+            nextBtn.onclick = function () {
                 if (_this.isRandom) {
                     _this.playRandomSong();
                 } else {
                     _this.nextSong();
                 }
                 audio.play();
-            }
+            };
             // Khi prev song
-            prevBtn.onclick = function(){
+            prevBtn.onclick = function () {
                 if (_this.isRandom) {
                     _this.playRandomSong();
                 } else {
                     _this.prevSong();
                 }
                 audio.play();
-            }
+            };
             // Xử lý bật / tắt random song
-            randomBtn.onclick = function(){
+            randomBtn.onclick = function () {
                 _this.isRandom = !_this.isRandom;
-                _this.setConfig('isRandom', _this.isRandom);
-                randomBtn.classList.toggle('active', _this.isRandom);
-            }
+                _this.setConfig("isRandom", _this.isRandom);
+                randomBtn.classList.toggle("active", _this.isRandom);
+            };
             // Xử lý next song khi audio ended
             audio.onended = function () {
                 if (_this.isRepeat) {
-                audio.play();
+                    audio.play();
                 } else {
-                nextBtn.click();
+                    nextBtn.click();
                 }
-            }
+            };
             // Xử lý lặp lại một song
             repeatBtn.onclick = function () {
                 _this.isRepeat = !_this.isRepeat;
-                _this.setConfig('isRepeat', _this.isRepeat);
+                _this.setConfig("isRepeat", _this.isRepeat);
                 repeatBtn.classList.toggle("active", _this.isRepeat);
-            }
+            };
             playlist.onclick = function (e) {
-                let songNode = e.target.closest('.song:not(.active)');
-                if (!e.target.closest('.option')) {
+                let songNode = e.target.closest(".song:not(.active)");
+                if (!e.target.closest(".option")) {
                     if (songNode) {
                         _this.currentIndex = Number(songNode.dataset.index);
                         _this.loadCurrentSong();
                         audio.play();
                     }
                 }
-            }
+            };
         },
 
-        loadCurrentSong () {
+        loadCurrentSong() {
             heading.textContent = this.currentSong.name;
             cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
             audio.src = this.currentSong.path;
-            currentTime.innerHTML = '00:00';
+            currentTime.innerHTML = "00:00";
             setTimeout(() => {
                 musicDuration.innerHTML = formatTime(audio.duration);
             }, 300);
 
-            if ($('.song.active')) 
-            {
-                $('.song.active').classList.remove('active');
+            if ($(".song.active")) {
+                $(".song.active").classList.remove("active");
             }
-            $$('.song')[app.currentIndex].classList.add('active')
+            $$(".song")[app.currentIndex].classList.add("active");
         },
-        loadConfig () {
+        loadConfig() {
             this.isRandom = this.config.isRandom;
             this.isRepeat = this.config.isRepeat;
         },
         nextSong: function () {
-            this.currentIndex++
-            if(this.currentIndex>=this.songs.length){
+            this.currentIndex++;
+            if (this.currentIndex >= this.songs.length) {
                 this.currentIndex = 0;
             }
             this.loadCurrentSong();
@@ -285,7 +282,7 @@ window.addEventListener('load', function(){
             setTimeout(() => {
                 $(".song.active").scrollIntoView({
                     behavior: "smooth",
-                    block: "end"
+                    block: "end",
                 });
             }, 300);
         },
@@ -300,10 +297,10 @@ window.addEventListener('load', function(){
             // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
             this.loadCurrentSong();
 
-            randomBtn.classList.toggle('active', this.isRandom);
-            repeatBtn.classList.toggle('active', this.isRepeat);
-        }
-    }
+            randomBtn.classList.toggle("active", this.isRandom);
+            repeatBtn.classList.toggle("active", this.isRepeat);
+        },
+    };
 
-    app.start()
-})
+    app.start();
+});
